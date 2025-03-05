9930030b9eb0539dd2fabe4e3220ebb590173ff0
qutibaalqasim <qalqasim9@gmail.com>
2025-03-05 03:11:15 +0300

    creating user.model

---
+++ DB/models/user.model.js
@@ -0,0 +1,53 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema({
+
+    userName:{
+        type: String,
+        required: true, 
+        min: 3,
+        max: 30,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: true,
+        min: 4,
+    },
+    image:{
+        type: Object,
+    },
+    phone:{
+        type: String,
+    },
+    address:{
+        type: String,
+    },
+    confirmEmail:{
+        type: Boolean,
+        default: false,
+    },
+    gender:{
+        type: String,
+        enum: ["male", "female"],
+    },
+    status:{
+        type: String,
+        enum: ["active", "inactive"],
+    },
+    role:{
+        type: String,
+        enum: ["user", "admin"],
+        default: "user",
+    }
+},{
+    timestamps: true,
+});
+
+
+const userModel =mongoose.models.User || model('User', userSchema);
+export default userModel;
