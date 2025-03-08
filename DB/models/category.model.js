efa9e440ecc591c75b835f0d79667f76f82eb9e1
qutibaalqasim <qalqasim9@gmail.com>
2025-03-08 22:54:59 +0300

    creating category model and category module

---
+++ DB/models/category.model.js
@@ -0,0 +1,37 @@
+import mongoose, { model, Schema, Types } from "mongoose";
+
+const categorySchema = new Schema({
+
+    name:{
+        type: String,
+        required: true, 
+        min: 3,
+        max: 60,
+    },
+    slug:{
+        type: String,
+        required: true,
+    },
+    image:{
+        type: Object,
+    },
+    status:{
+        type: String,
+        default : "active",
+        enum: ["active", "inactive"],
+    },
+    createdBy:{
+        type: Types.ObjectId,
+        ref: "User",
+    },
+    updatedBy:{
+        type: Types.ObjectId,
+        ref: "User",
+    }
+},{
+    timestamps: true,
+});
+
+
+const categoryModel = mongoose.models.User || model('Category', categorySchema);
+export default categoryModel;
