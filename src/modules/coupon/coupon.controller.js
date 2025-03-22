a9a2e1791444bab0fbf8b426b8d099e8bd3b0526
qutibaalqasim <qalqasim9@gmail.com>
2025-03-22 23:54:28 +0300

    creating getCoupons , getCouponDetails and updateCoupon end points

--- src/modules/coupon/coupon.controller.js
+++ src/modules/coupon/coupon.controller.js
@@ -14,3 +14,38 @@ export const createCoupon = async (req,res,next)=>{
    return res.status(201).json({message:"success" , coupon});
 
 }
+export const getCoupons = async (req,res,next)=>{
+    const coupons = await couponModel.find({});
+    return res.status(200).json({message:"success" , coupons});
+}
+
+export const getCouponDetails= async (req, res, next) => {
+    const {id} = req.params;
+    const coupon = await couponModel.findById(id);
+    if(!coupon){
+        return res.status(404).json({message:"coupon not found"});
+    }
+
+    return res.status(200).json({message:"success", coupon});
+}
+
+export const updateCoupon= async (req, res, next) => {
+    const {id} = req.params;
+    const {name} = req.body;
+    const coupon = await couponModel.findById(id);
+    if(!coupon){
+        return res.status(404).json({message:"coupon not found"});
+    }
+
+    const checkName = await couponModel.findOne({name});
+    if(checkName){
+        return res.status(409).json({message:"this coupon name already exists!!"});
+    }
+
+    coupon.name = name;
+    coupon.updatedBy = req.id;
+    coupon.createdBy = req.id;
+    coupon.status = req.body.status;
+    await coupon.save();
+    return res.status(200).json({message:"success", coupon});
+}
--- src/modules/coupon/coupon.router.js
+++ src/modules/coupon/coupon.router.js
@@ -6,7 +6,8 @@ import { auth } from "../../middleware/auth.js";
 const router = Router();
 
 router.post('/' , auth(['admin']) , controller.createCoupon);
-
-
+router.get('/' , auth(['admin']) , controller.getCoupons);
+router.get('/:id', auth(['admin']) , controller.getCouponDetails);
+router.put('/:id', auth(['admin']) , controller.updateCoupon);
 
 export default router;
